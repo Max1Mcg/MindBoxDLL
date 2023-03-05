2256b2a6a64ffc18b449c530412a2f534a6039bb
Max1M <leggo2001@mail.ru>
2023-03-05 12:56:08 +0300

    Area class now create new Figure and return .GetArea() for instance instead of work with reflection.

--- MindBoxDLL.tests/MindBoxDLL.cs
+++ MindBoxDLL.tests/MindBoxDLL.cs
@@ -19,11 +19,12 @@ namespace MindBoxDLL.Tests
                 ThrowsException.Equals(e.Message, " руга с таким радиусом не бывает");
             }
         }
+        //ћетод дл€ получени€ площади фигуры, не зна€ еЄ типа во врем€ компил€ции, формула
+        //дл€ площади дл€ OtherFigure задаетс€ пользователем
         [Fact]
         public void OtherFigureArea()
         {
-            Object o = new OtherFigure("MyFigure",new List<double>() { 1,2,3}, "!0 * (!1+!2)");
-            Assert.Equal(5, Area.GetArea(o));
+            Assert.Equal(5, Area.GetArea("MyFigure", new List<double>() { 1, 2, 3 }, "!0 * (!1+!2)"));
         }
     }
 }
--- MindBoxDLL/Area.cs
+++ MindBoxDLL/Area.cs
@@ -10,15 +10,38 @@ namespace MindBoxDLL
     //объект которого является производным от Figure или нет)
     public static class Area
     {
-        public static double GetArea(Object o)
+        static readonly System.Collections.Generic.Dictionary<string, Type> d = null;
+        static Area()
         {
-            if (o is Circle c)
-                return c.GetArea();
-            else if (o is Triangle t)
-                return t.GetArea();
-            else if (o is OtherFigure of)
-                return of.GetArea();
-            throw new InvalidCastException("Данный тип не является производным от Figure");
+            d = new System.Collections.Generic.Dictionary<string, Type>() { { "Circle", typeof(Circle)}, { "Triangle", typeof(Triangle)}, {"OtherFigure", typeof(OtherFigure)} };
         }
+        public static double GetArea(string name, List<double>sides, string formulaForArea = "")
+        {
+            Figure figure = null;
+            switch (name)
+            {
+                case "Circle":
+                    figure = new Circle(sides[0]);
+                    break;
+                case "Triangle":
+                    figure = new Triangle(sides[0], sides[1], sides[2]);
+                    break;
+                default:
+                    figure = new OtherFigure(name, sides, formulaForArea);
+                    break;
+            }
+            return figure.GetArea();
+        }
+        //Тип для этого метода извествен в compile-time`e
+        //public static double GetArea(Object o)
+        //{
+        //    if (o is Circle c)
+        //        return c.GetArea();
+        //    else if (o is Triangle t)
+        //        return t.GetArea();
+        //    else if (o is OtherFigure of)
+        //        return of.GetArea();
+        //    throw new InvalidCastException("Данный тип не является производным от Figure");
+        //}
     }
 }
